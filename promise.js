f5dbaafc2c1f037c80c4e86b7cc73f969af70a79
Gunawan Prasetia <gunawan.ep@gmail.com>
2022-09-18 08:51:09 +0700

    tambah fungsi menghitung jumlah film berdasarkan emosi penontonnya

--- promise.js
+++ promise.js
@@ -1,7 +1,11 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+ let movieList = [].concat(await promiseTheaterIXX(), await promiseTheaterVGC());
+ 
+ return movieList.filter((movie) => movie.hasil === emotion).length;
+};
 
 module.exports = {
   promiseOutput,
